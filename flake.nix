65ec4e080b3480167fc1a748c89a05901eea9a9b
David Arnold <david.arnold@iohk.io>
2023-02-25 01:27:04 -0500

    fix: file name

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Small set of shared base actions for Paisano-based projects.";
 
   outputs = {nixpkgs, ...}: let
-    mkCommand = import ./mkCommand.nix {inherit nixpkgs;};
+    mkCommand = import ./command.nix {inherit nixpkgs;};
     actions = import ./actions.nix {
       inherit (nixpkgs) lib;
       inherit nixpkgs mkCommand;
